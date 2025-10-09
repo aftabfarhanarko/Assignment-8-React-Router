dde582df1037f29e68ddede1d71116e9f987766b
Aftab Farhan <aftabfarhan324@gmail.com>
2025-10-10 03:29:58 +0600

    addes hjover

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import Display from "./Display";
 import TrindingApp from "../TrindingApp/TrindingApp";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
 import AppLoder from "../../Loder/AppLoder";
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div>
       <Display></Display>
-      <div>
+      <div className="max-w-[1250px] mx-auto">
         <div className="text-center mt-20">
           <h1 className="text-4xl md:text-5xl font-semibold">Trending Apps</h1>
           <p className="text-lg text-gray-400 mt-3">
@@ -19,13 +19,20 @@ const Home = () => {
           </p>
         </div>
 
-      <div className="grid grid-cols-1  lg:grid-cols-4 md:grid-cols-2">
+        <div className="grid grid-cols-1  lg:grid-cols-4 md:grid-cols-2 gap-7 px-4 md:px-0">
           {appData.map((data) => (
-          <Suspense fallback={<AppLoder></AppLoder>}>
-            <TrindingApp data={data}></TrindingApp>
-          </Suspense>
-        ))}
-      </div>
+            <Suspense fallback={<AppLoder></AppLoder>}>
+              <TrindingApp data={data}></TrindingApp>
+            </Suspense>
+          ))}
+        </div>
+        <div className="flex justify-center mt-15 ">
+          <Link to="">
+          <button className="  shadow-2xl btn bg-gradient-to-tr from-[#632EE3] to-[#9F62F2] text-white  text-center  px-12 text-lg">
+            Show All
+          </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
