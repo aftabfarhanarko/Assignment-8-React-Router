db5016e6e00f9110564f7301228692e9a7a9d451
Aftab Farhan <aftabfarhan324@gmail.com>
2025-10-11 01:55:38 +0600

    sort implement

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -31,6 +31,7 @@ export const router = createBrowserRouter([
       },
       {
         path: "/install",
+        loader: () => fetch("/allapp.json"),
         Component: InstallsApp,
       },
       {
